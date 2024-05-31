a4231ba1d90030291ceaf809490efd779b238a14
imokoi <tiantengfei@outlook.com>
2024-05-31 11:55:32 +0800

    update link preview

--- core/src/models/link_preview.rs
+++ core/src/models/link_preview.rs
@@ -1,10 +1,17 @@
+use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq, Eq, Builder)]
+#[builder(setter(into), default)]
 pub struct LinkPreviewOptions {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub is_disabled: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub prefer_small_media: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub prefer_large_media: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub show_above_text: Option<bool>,
 }
