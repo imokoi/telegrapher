02491b2d67633f14b0a593985ebdf04b674290ad
imokoi <tiantengfei@outlook.com>
2024-08-02 00:53:26 +0800

    update

--- examples/send_document.rs
+++ examples/send_document.rs
@@ -5,7 +5,7 @@ use telegrapher::params::media_params::SendDocumentParamsBuilder;
 async fn main() {
     let bot = Bot::new("6616659571:AAEr0TdwPXBnvHQl_VJj5Z6wh-p3uUDNbOw", 1);
     let send_document_params = SendDocumentParamsBuilder::default()
-        .chat_id("1393242628")
+        .chat_id(1393242628)
         .document(FileUpload::String("https://cf-ipfs.com/ipfs/QmP3R6KfKKH1C4gBSnGg7kWgPK4gnMxs74bR23QrTUezow?random=171988623".to_string()))
         .build()
         .unwrap();
